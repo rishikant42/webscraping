56474a44aa4aa1cf850445061e6e178282744c5c
rishikant <rksbtp@gmail.com>
2017-05-28 20:56:33 +0530

    check & remove duplicate entries from db

--- webscrapingapp/views.py
+++ webscrapingapp/views.py
@@ -11,6 +11,8 @@ def search_form(request):
 
 def search(request):
     q = request.GET['q']
+
+    # Extract searched queries from DB
     queries = Query.objects.values_list('query', flat=True)
 
     if q in queries:
@@ -28,19 +30,28 @@ def search(request):
         links = soup.find_all("a")
         links_text = [link.text for link in links]
 
+        # Extract appName & link address from links_text
         for i in range(len(links_text)):
             if links_text[i] == '  Pre-ordered    ':
                 appName.append(links_text[i+2])
                 href.append(links[i+2].get("href"))
                 
+        # Extract appID from links
         appID = [i.split("id=")[1] for i in href]
 
+        # making url address for apps
         websites = ["https://play.google.com"+i for i in href][0:10]
 
         for i in range(10):
             data[appName[i]] = ["AppID: " + appID[i], "Website: " + websites[i]]
 
+        # save new query in DB
         new_query = Query(query=q,results=data)
         new_query.save()
+
+    # delete duplicate entries from DB
+    for row in Query.objects.filter(query=q):
+        if Query.objects.filter(query=q).count() > 1:
+            row.delete()
 
     return render(request, 'search_results.html', {'data':data, 'query':q})

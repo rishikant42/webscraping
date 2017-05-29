7c85c9a27c9b12eb94af6c9ff7d3924c5f30ca79
rishikant <rksbtp@gmail.com>
2017-05-29 22:42:23 +0530

    split search fn into different fn

--- webscrapingapp/views.py
+++ webscrapingapp/views.py
@@ -9,6 +9,36 @@ import ast
 def search_form(request):
     return render(request, 'search_form.html')
 
+def email_scrap(websites):
+    mailto = []
+    for url in websites:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        mail_link = soup.find_all("a", {"class":"dev-link"}) 
+
+        if not mail_link:
+            mailto.append('')
+        else:
+            for link in mail_link:
+                if "mailto" in link.attrs.get("href"):
+                    mailto.append(link.attrs.get("href"))
+    emails = []
+    for i in mailto:
+        if i != '':
+            emails.append(i.replace("mailto:", "Email: "))
+        else:
+            emails.append('')
+    return emails
+
+def app_name_href_scrap(links):
+    appName = []
+    href = []
+    for link in links:
+        href.append(link.attrs.get("href"))
+        appName.append(link.attrs.get("title"))
+    return(appName, href)
+
+
 def search(request):
     q = request.GET['q']
     queries = Query.objects.values_list('query', flat=True)
@@ -19,8 +49,6 @@ def search(request):
         weburl = ast.literal_eval(query.weburl)
     else:
         data = {}
-        appName = []
-        href = []
 
         url = "https://play.google.com/store/search?q=%s" % q
         r = requests.get(url)
@@ -30,32 +58,13 @@ def search(request):
 
         links = links[0:10]
 
-        for link in links:
-            href.append(link.attrs.get("href"))
-            appName.append(link.attrs.get("title"))
+        appName, href = app_name_href_scrap(links)
 
         appID = [i.split("id=")[1] for i in href]
 
         websites = ["https://play.google.com"+i for i in href][0:10]
 
-        mailto = []
-        for url in websites:
-            p = requests.get(url)
-            sup = BeautifulSoup(p.content)
-            mail_link = sup.find_all("a", {"class":"dev-link"}) 
-
-            if not mail_link:
-                mailto.append('')
-            else:
-                for d in mail_link:
-                    if "mailto" in d.attrs.get("href"):
-                        mailto.append(d.attrs.get("href"))
-        emails = []
-        for i in mailto:
-            if i != '':
-                emails.append(i.replace("mailto:", "Email: "))
-            else:
-                emails.append('')
+        emails = email_scrap(websites)
 
         for i in range(10):
             data[appName[i]] = ["AppID: " + appID[i], "Website: " + websites[i], emails[i]]
